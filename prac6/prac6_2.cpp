9b083e3399645e23dce898f3f0caa13a5336939b
Caesar01108 <76813100+Caesar01108@users.noreply.github.com>
2021-08-27 17:34:38 +0530

    Update prac6_2.cpp

--- prac6/prac6_2.cpp
+++ prac6/prac6_2.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+// FUNCTION INSIDE THE CLASS:
+#include<iostream>
+using namespace std; 
+class Box{
+    int l,b,h;
+    public:
+    void assignval(){
+        cout<<"Length:";cin>>l;
+        cout<<"Breadth:";cin>>b;
+        cout<<"Height:";cin>>h;
 
-using namespace std;
+    }
+    void printval(){
+        cout<<"\nLength is :"<<l<<"\nBreadthis :"<<b<<"\nHeightis :"<<h<<endl;
+    }
+};
+int main(){
+    Box B[4];
+    for(int i=0;i<4;i++){
+        cout<<"For Box "<<i+1<<endl;
+        B[i].assignval();
+        B[i].printval();
 
-// array of four boxes
-// TODO:
+    }
+}
