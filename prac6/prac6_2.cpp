0e5f7cb3311f9fc7ca872f185715513cbd7f6ff8
Caesar01108 <76813100+Caesar01108@users.noreply.github.com>
2021-08-27 16:44:19 +0530

    Update prac6_2.cpp

--- prac6/prac6_2.cpp
+++ prac6/prac6_2.cpp
@@ -1,6 +1,62 @@
-#include <iostream>
+// FUNCTION INSIDE THE CLASS:
+// #include<iostream>
+// using namespace std;
+// class Box1
+// { 
+// public:
+//   int length,breadth,height;
+//     void takeData(){
+//         cout<<"Enter your length: ";
+//         cin>>length;
+//         cout<<"Enter your breadth: ";
+//         cin>>breadth;
+//         cout<<"Enter your height: ";
+//         cin>>height;
+//     }
+//     void giveData(){
+//         cout<<"\nLength is :"<<length;
+//         cout<<"\nBreadth is :"<<breadth;
+//         cout<<"\nHeight is :"<<height;
+//     }
+// };
+//  int main(){
+//     Box1 a;
+//     a.takeData();
+//     a.giveData();
+    
+// }
 
-using namespace std;
+// FUNCTION OUTSIDE THE CLASS:
+// class Box2
+// { 
+// public:
+//   int length,breadth,height;
+//   void takeData();
+//   void giveData();
+// };
+//     void Box2:: takeData(){
+//         cout<<"\nEnter your length: ";
+//         cin>>length;
+//         cout<<"Enter your breadth: ";
+//         cin>>breadth;
+//         cout<<"Enter your height: ";
+//         cin>>height;
+//     }
+//     void  Box2::giveData(){
+//         cout<<"\nLength is :"<<length;
+//         cout<<"\nBreadth is :"<<breadth;
+//         cout<<"\nHeight is :"<<height;
+//     }
 
-// array of four boxes
-// TODO:
+//  int main(){
+//     int i,num;
+//     cout<<"Enter how many u want: ";
+//     cin>>num;
+//     Box a[num]; 
+  
+//     for ( i = 0; i < num; i++)
+//     {
+//     a[i].takeData();
+//     a[i].giveData();
+//     }
+// } 
